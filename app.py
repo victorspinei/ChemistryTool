ba2610654adfcf654f7f807c6e31c8a15ce2d65e
victor247k <victor.clash.3017@gmail.com>
2023-10-25 18:19:39 +0300

    api done

--- app.py
+++ app.py
@@ -2,26 +2,45 @@ from flask import Flask, request, jsonify
 
 import json
 
-
 app = Flask(__name__)
 
+def find_element(symbol):
+    """here we will find the element from the list and return the info about it"""
+    with open("elements.json", "r") as file:
+        elemets = json.load(file)
 
+    for element in elemets:
+        if element['symbol'] == symbol:
+            return element
+
+    return None
 
 @app.route("/")
 def index():
     """Landing page"""
     return "<h1>Hello from flask</h1>"
 
-@app.route("/api")
-def element():
-    element = request.args.get('element')
-    method = request.args.get('m')
+# Route to get a single element
+@app.route("/api/elements/<symbol>")
+def get_single_element(symbol):
+    element = find_element(symbol)
+    
+    if element:
+        return jsonify(element)
+    else:
+        return jsonify({"message": "Element not found."}), 404
 
-    return "TODO!"
+# Route to get multiple elements
+# /api/elements?symbol=He&symbol=O
+@app.route("/api/elements")
+def get_multiple_elements():
+    symbols = request.args.getlist('symbol')  # Get a list of symbols from the query parameters
+    elements = [find_element(symbol) for symbol in symbols]
+    return jsonify(elements)
 
-@app.route("/all")
-def get_all():
+# Route to get all elements
+@app.route("/api/all")
+def get_all_elements():
     with open("elements.json", "r") as file:
         elements = json.load(file)
-
     return jsonify(elements)
