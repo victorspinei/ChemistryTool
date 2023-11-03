592af6546c5f348e9e358f3d83ec597dc28fac77
victor247k <victor.clash.3017@gmail.com>
2023-11-03 16:57:34 +0200

    added a whole lot of stuff

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 def find_element(symbol):
     """here we will find the element from the list and return the info about it"""
@@ -20,15 +22,10 @@ def index():
     """Landing page"""
     return render_template("index.html")
 
-# Route to get a single element
-@app.route("/api/elements/<symbol>")
-def get_single_element(symbol):
-    element = find_element(symbol)
-    
-    if element:
-        return jsonify(element)
-    else:
-        return jsonify({"message": "Element not found."}), 404
+@app.route("/apps")
+def apps():
+    """Applixations page"""
+    return render_template("apps.html")
 
 # Route to get multiple elements
 # /api/elements?symbol=He&symbol=O
@@ -47,3 +44,4 @@ def get_all_elements():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
---
+++ static/apps.js
@@ -0,0 +1,94 @@
+const inputElement = document.getElementById("inputElement");
+const outputElement = document.getElementById("outputElement");
+const buttonElement = document.getElementById("buttonElement");
+
+let AllElements = []
+
+async function fetchAndSave() {
+    try {
+        const response = await fetch("http://127.0.0.1:5000/api/all");
+        const data = await response.json();
+        AllElements = data; // Assuming AllElements is defined outside this function
+
+        // Now you can use AllElements here or return it, depending on your needs
+    } catch (error) {
+        console.error("Error getting the elements: ", error);
+    }
+}
+
+async function fetchData() {
+    await fetchAndSave()
+    buttonElement.addEventListener('click', function(){
+        const value = inputElement.value
+        if (!value) return;
+
+
+        const elements = parseChemicalFormula(value);
+
+        
+        let sum = 0.00;
+
+        for (let symbol in elements) {
+            let element = findElement(symbol);
+
+            // if (!element) {
+            //     outputElement.value = "Error";
+            //     return;
+            // }
+            try {
+                sum += parseFloat(element['atomic_weight']) * elements[symbol];
+            } catch (error) {
+                sum += 0;
+            }
+        }
+
+        outputElement.value = sum.toFixed(2) + " g/mol" 
+    });
+    function findElement(symbol) {
+        for (let i = 0; i < AllElements.length; i++) {
+            if (symbol === AllElements[i]['symbol']) {
+                return AllElements[i];
+            }
+        } 
+        return null;
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetchData();
+});
+
+fetchData()
+
+function parseChemicalFormula(formula) {
+  const regex = /([A-Z][a-z]*)(\d*)/g;
+  let elements = {};
+  let match;
+
+  while ((match = regex.exec(formula)) !== null) {
+    const element = match[1];
+    const count = match[2] === "" ? 1 : parseInt(match[2], 10);
+    if (elements[element]) {
+      elements[element] += count;
+    } else {
+      elements[element] = count;
+    }
+  }
+
+  return elements;
+}
+
+const buttons = document.querySelectorAll('.e')
+const modal = document.querySelector('#myModal');
+
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log(btn.querySelector('abbr').innerText)
+        
+    })
+    modal.addEventListener('shown.bs.modal', () => {
+    btn.focus()
+    })
+});
